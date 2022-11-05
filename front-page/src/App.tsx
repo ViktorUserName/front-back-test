59a8630270d4c24b5d6c88b61fe5329a9d0d7a2d
ViktorUserName <vityachishevich@gmail.com>
2022-11-05 23:12:39 +0300

    theme content

--- front-page/src/App.tsx
+++ front-page/src/App.tsx
@@ -10,38 +10,64 @@ import Main from './pages/Main/Main';
 
 // import dataJson from './providerData/data.json'
 // import { DataProvider } from './providerData/providerFirst';
-const themes = {
-  light: {
-    background: "#eeeeee"
-  },
-  dark: {
-    background: "#222222"
-  }
-};
+// const themes = {
+//   light: {
+//     background: "#eeeeee"
+//   },
+//   dark: {
+//     background: "#222222"
+//   }
+// };
 
 // export const DataContext = createContext(themes.light);
 // const DataProvider = DataContext.Provider;
 // const DataConsumer = DataContext.Consumer
-export const ColorContext = createContext({});
-const ColorProvider = ColorContext.Provider
+// const ColorProvider = ColorContext.Provider
+type Theme = 'light' | 'dark';
+type ThemeContextType = {
+  theme: Theme;
+  changeTheme: (theme: Theme) => void;
+};
 
+const ThemeCotext = createContext<ThemeContextType | null>(null)
+const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({children}) => {
+  const [themeMode, setThemeMode] = useState<Theme>('light');
+  return (
+    <ThemeCotext.Provider value={{theme: themeMode, changeTheme: setThemeMode}}>
+      {children}
+    </ThemeCotext.Provider>
+  )
+}
 
-
+const ThemeWrapper:React.FC<{children: React.ReactNode}> = ({children}) => {
+  const {theme, changeTheme} = React.useContext(ThemeCotext) as ThemeContextType;
+  const handlerThemeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    changeTheme(event.target.value as Theme);
+  }
+  return (
+    <div className="Theme-wrapper" data-theme={theme}>
+      <select name="toggleTheme" onChange={handlerThemeChange} id="">
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+      {children}
+    </div>
+  )
+}
 function App() {
-  // let [colorContext, setColorContext] = useState(themes.light);
-  // let changeHandler = () => {
-  //   setColorContext(colorContext => colorContext=themes.dark)
-  // }
 
   return (
     <div className="App">
       <Header/>
-        <ColorProvider value={themes}>
-          <Routes>
+          {/* <Routes>
               <Route path='/' element={<Main/>}/>
               <Route path='/cards' element={<Sell/>}/>
-          </Routes>
-        </ColorProvider>
+          </Routes> */}
+          <ThemeProvider>
+            <ThemeWrapper>
+              <div className="testContent">test</div>
+            </ThemeWrapper>
+          </ThemeProvider>
     </div>
   );
 }
--- front-page/src/pages/Main/Main.tsx
+++ front-page/src/pages/Main/Main.tsx
@@ -1,18 +1,9 @@
 import React, {useContext, useState} from 'react';
 import s from './Main.module.scss';
-import { ColorContext } from '../../App';
+// import { ColorContext } from '../../App';
 
 const Main: React.FC = () => {
-    // const [state, setState] = useState(false)
-    // const data = useContext(DataContext)
-    // const DataConsumer = DataContext.Consumer
-    // const changeColor = () => {
-    //     setState(!state)
-    // }
-    const handler = () => {
-        console.log(color.dark)
-    }
-    const color = useContext(ColorContext)
+
     return (
         <div className={s.main}>
             {/* {<p>{data.background}</p>}
@@ -20,7 +11,7 @@ const Main: React.FC = () => {
                 {value => <div>{state &&value.background}</div>}
             </DataConsumer>
             <button onClick={changeColor}>set {`${state}`}</button> */}
-             <button onClick={handler}>set</button>
+             <button>set</button>
             {/* {console.log('hi')} */}
         </div>
     );
