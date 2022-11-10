e45492d7eb7d8d361461b6412ddac843d0ee316b
ViktorUserName <vityachishevich@gmail.com>
2022-11-10 03:37:44 +0300

    update some f

--- mongoapp/index.js
+++ mongoapp/index.js
@@ -1,38 +1,31 @@
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 
-const url = "mongodb://0.0.0.0:27017/"
-const mongoClient = new MongoClient(url);
-mongoClient.connect(function(err, client) {
-    if (err){return console.log(err)};
-    // client.close();
-    const db = client.db("admin");
-     
-    db.command({ping: 1}, function(err, result){
-        if(!err){
-            console.log("Подключение с сервером успешно установлено");
-            console.log(result);
-        }
-    client.close()
-    console.log('close')
-    })
-})
+const url = "mongodb://0.0.0.0:27017/";
+const client = new MongoClient(url, { monitorCommands:true });
+const dbname = 'cardsdb';
 
-// const url = "mongodb://localhost:27017/";
-// const mongoClient = new MongoClient(url);
+
+async function main() {
+    await client.connect();
+    console.log('connect successfull');
+    const db = client.db(dbname)
+    const collection = db.collection('card')
+    
+    // const test = await collection.findOneAndUpdate(
+    //     {id:1}, {$set: {price: '20000'}}, {returnDocument: 'after'}
+    //     )
+    // const test = await collection.find({}).project({id:1, _id:0}).toArray();
+    // const test = await client.db(dbname).collection('card').countDocuments()
+   // const ping = await db.command({ping:1});
+    console.log(test);
   
-// // Подключаемся к серверу
-// mongoClient.connect(function(err, client){
-       
-//     // обращаемся к базе данных admin
-//     const db = client.db("admin");
-     
-//     db.command({ping: 1}, function(err, result){
-//         if(!err){
-//             console.log("Подключение с сервером успешно установлено");
-//             console.log(result);
-//         }
-//         // Закрываем подключение
-//         client.close();
-//         console.log("Подключение закрыто");
-//     });
-// });
+    // console.log('here:', insertry);
+    
+    // console.log(ping);
+    
+}
+
+main()
+// .then(console.log)
+.catch(console.error)
+.finally(() => client.close())
